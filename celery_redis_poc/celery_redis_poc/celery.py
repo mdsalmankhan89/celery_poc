1d470f0683c636824409427ee4ec1b544731b706
Mohammad Salman Khan <mdsalmankhan89@gmail.com>
2021-06-12 16:34:38 +0530

    poc1

--- celery_redis_poc/celery_redis_poc/celery.py
+++ celery_redis_poc/celery_redis_poc/celery.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
-
+import time
+import shutil
+import os
 from celery import Celery
+from datetime import datetime
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_redis_poc.settings')
@@ -20,4 +23,19 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    print("in debug task")
+    #print('Request: {0!r}'.format(self.request))
+
+@app.task(bind=True)
+def copy_file(self):
+    #src = "C:\\Users\\salman\\Desktop\\LinkedIn Learning\\Python\\Ex_Files_Learning_Python_Upd\\Exercise Files\\source\\file.txt"
+    #dest = "C:\\Users\\salman\\Desktop\\LinkedIn Learning\\Python\\Ex_Files_Learning_Python_Upd\\Exercise Files\\destination\\file.txt" 
+    #dir = "fff"
+    curDT = datetime.now()
+    date_time = curDT.strftime("%m-%d-%Y %H_%M_%S")
+    dest = "C:\\Users\\salman\\Desktop\\LinkedIn Learning\\Python\\Ex_Files_Learning_Python_Upd\\Exercise Files\\destination"
+    path = os.path.join(dest, date_time)
+    time.sleep(10);
+    os.mkdir(path)
+    #shutil.copyfile(src, dest)
+    print("hello1")
